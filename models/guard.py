85c0bb3049e6363ed785d56cce3e37aecebbe953
MayMay <maylis.baschet@gmail.com>
2019-05-02 20:53:45 +0200

    guard

--- models/guard.py
+++ models/guard.py
@@ -13,20 +13,18 @@ class Guard:
     def __init__(self, map):
         self.map = map
         self.position = self.map.exit   # N.B: starts with 0
+        # say the guard is in the exit cause
+        # hero can pass through it but can't go bout
+        # will guard stairs for example
 
-        self.test_map_sides()
-
-    def test_map_sides(self):
-        """ check where to put the guard in the game"""
-        pass
-        # if self.position.right not in self.map:
-        #     if self .position.left not in self.map :
-
+    @property
+    def get_position(self):
+        return self.position
 
 def main():
     game = Map('data/maps/map.txt')
     g = Guard(game)
-    print(g.test_map_sides())
+    print(g.get_position)
 
 if __name__ == "__main__":
     main()
