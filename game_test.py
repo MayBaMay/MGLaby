9e55fc8da287d44a5494dce886cf626908308775
MayMay <maylis.baschet@gmail.com>
2019-05-02 23:15:35 +0200

    prog test avec fonctions

--- game_test.py
+++ game_test.py
@@ -6,11 +6,6 @@ from models.position import Position
 from models.syringe import Syringe
 from models.characters import Characters, Hero, Guard
 
-map = Map('data/maps/map_test.txt')
-hero = Hero(map)
-guard = Guard(map)
-sy = Syringe(map, hero)
-
 def user_answer():
     user = input("Tapez I(up) J(left) K(down) L(left) Q(quittez)   ")
     user = user.upper()
@@ -25,18 +20,28 @@ def user_answer():
     print("MacGyver is here : {}".format(hero.get_position))
     return user
 
-user = ""
-
-while user != "Q" :
-    user = user_answer()
-
+def get_syringe():
     if hero.get_position in sy.get_positions:
         print("you have found the {}, keep looking!".format(sy.interaction_hero()))
         if sy.check_making() == True :
             print("Bravo, you have the syringe, find the guardian!")
 
+def goal_case():
     if hero.get_position == guard.get_position:
         if sy.check_making() == True :
             print("YOU WIN !!!")
+            return "Q"
         else :
             print("YOU SHALL NOT PASS !!!")
+
+
+map = Map('data/maps/map_test.txt')
+hero = Hero(map)
+guard = Guard(map)
+sy = Syringe(map, hero)
+user = ""
+
+while user != "Q" :
+    user = user_answer()
+    get_syringe()
+    user = goal_case()
