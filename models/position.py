f1198b021a87101222ee467fe4a52a2f7ce9b2ba
MayBaMay <49728619+MayBaMay@users.noreply.github.com>
2019-05-15 13:52:32 +0200

    doc&comments

--- models/position.py
+++ models/position.py
@@ -1,5 +1,7 @@
 
-""" This module allows all object in the programm to heve a position"""
+""" This module allows all objects in the programm to have a position
+This position is encapsulated in an independant class.
+That will allow us to use methods directly to those positions."""
 
 import config.settings as constants
 
@@ -13,31 +15,31 @@ class Position :
 
     def __eq__(self, pos):
         if isinstance(pos, Position): # if object still in the game
-            # compare two object of this class with their attribut ".position"
+            # compare two objects of this class with their attribute ".position"
             return self.position == pos.position
 
     def up(self):
-        """ position lifted by 1 case"""
+        """ position lifted by 1 sprite """
         x, y = self.position
         return self.__class__(x, (y-constants.SPRITES_SIZE))
 
     def down(self):
-        """ position lowered by 1 case"""
+        """ position lowered by 1 sprite"""
         x, y = self.position
         return self.__class__(x, (y+constants.SPRITES_SIZE))
 
     def right(self):
-        """ position moved by 1 case towards the right """
+        """ position moved by 1 sprite towards the right """
         x, y = self.position
         return self.__class__((x+constants.SPRITES_SIZE), y)
 
     def left(self):
-        """ position moved by 1 case towards the left """
+        """ position moved by 1 sprite towards the left """
         x, y = self.position
         return self.__class__((x-constants.SPRITES_SIZE), y)
 
     @property
     def get_position(self):
-        """ returns position in the map as a coordinate tuple"""
+        """ returns position in the map as a coordinate tuple (x, y)"""
         x, y = self.position
         return (x, y)
